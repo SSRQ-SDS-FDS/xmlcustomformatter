ecdfe0ec8038b6f55497042d3b3b42921600ed90
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-08-10 17:49:42 +0200

    Refactoring and Element node Processing

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from xmlformattingoptions import XMLFormattingOptions
 from xmlcustomformatter import XMLCustomFormatter
 
-list_of_tags = ['inline-element', 'empty-element']
+list_of_tags = ['inline-element']
 formatting_options = XMLFormattingOptions(
     indentation=4,
     inline_elements=list_of_tags)
--- xmlcustomformatter.py
+++ xmlcustomformatter.py
@@ -65,18 +65,14 @@ class XMLCustomFormatter:
         self.result.append(' ' + node.nodeName + '="' + node.nodeValue + '"')
 
     def process_cdata_node(self, node):
-        self.result.append(self.calculate_indentation())
         self.result.append("\n" + self.calculate_indentation() + "<![CDATA[" + node.nodeValue + ']]>\n')
 
     def process_comment_node(self, node):
-        comment = node.data
-        comment = comment.replace('\n', ' ')
-        comment = regex.sub(r'\s+', ' ', comment)
-        self.result.append('\n' + self.calculate_indentation() + '<!--' + comment + '-->\n')
+        node.data = self.reduce_redundant_whitespace(node.data)
+        self.result.append('\n' + self.calculate_indentation() + '<!--' + node.data + '-->\n')
 
     def process_document_node(self, node):
-        for childNode in node.childNodes:
-            self.process(childNode)
+        self.process_children(node)
 
     def process_document_type_node(self, node):
         self.result.append("<!DOCTYPE " + node.name + " ")
@@ -100,81 +96,130 @@ class XMLCustomFormatter:
             self.result.append('[' + subset + '\n]')
             self.decrease_indentation_level()
 
+    def process_element_node(self, node):
+        self.process_element_start_tag(node)
+        if not self.is_empty_element(node):
+            self.process_children(node)
+            self.process_element_end_tag(node)
+
+    def process_element_start_tag(self, node):
+        if self.is_inline_element(node):
+            self.process_inline_element_start_tag(node)
+        else:
+            self.process_container_element_start_tag(node)
+
+    def process_container_element_start_tag(self, node):
+        self.result.append('\n' + self.calculate_indentation() + '<' + node.tagName)
+        self.process_element_attributes(node)
+        self.close_start_tag(node)
+        self.result.append('\n')
+        if not self.is_empty_element(node):
+            self.increase_indentation_level()
+
+    def process_inline_element_start_tag(self, node):
+        self.determine_inline_element_indentation(node)
+        self.open_start_tag(node)
+        self.process_element_attributes(node)
+        self.close_start_tag(node)
+
+    def determine_inline_element_indentation(self, node):
+        # Depending on its context in the parent node, an inline-element has to be indented.
+        previous = node.previousSibling
+        # Indent if it is the first child of its parent
+        if previous is None:
+            self.result.append(self.calculate_indentation())
+        # Indent if it follows immediately after a comment (4), processing instruction (7) or cdata (8) node
+        elif previous.nodeType in (4, 7, 8):
+            self.result.append(self.calculate_indentation())
+        # Indent if it follows a text node (3) consisting of whitespace that follows a comment or cdata node
+        elif (previous.nodeType == 3 and regex.match(r"\s+", previous.data)) \
+                and (previous.previousSibling is not None and previous.previousSibling.nodeType in (4, 7, 8)):
+            self.result.append(self.calculate_indentation())
+
+    def open_start_tag(self, node):
+        self.result.append('<' + node.tagName)
+
+    def close_start_tag(self, node):
+        if self.is_empty_element(node):
+            self.result.append('/>')
+        else:
+            self.result.append('>')
+
+    def process_element_attributes(self, node):
+        if node.hasAttributes:
+            for i in range(node.attributes.length):
+                self.process(node.attributes.item(i))
+
+    def process_children(self, node):
+        for childNode in node.childNodes:
+            self.process(childNode)
+
+    def process_element_end_tag(self, node):
+        if self.is_inline_element(node):
+            self.process_inline_element_end_tag(node)
+        else:
+            self.process_container_element_end_tag(node)
+
+    def process_container_element_end_tag(self, node):
+        self.decrease_indentation_level()
+        self.result.append('\n' + self.calculate_indentation() + '</' + node.tagName + '>\n')
+
+    def process_inline_element_end_tag(self, node):
+        self.result.append('</' + node.tagName + '>')
+
+    def process_processing_instruction_node(self, node):
+        node.data = self.reduce_redundant_whitespace(node.data)
+        node.data = node.data.strip()
+        self.result.append('\n' + self.calculate_indentation() + '<?' + node.target + ' ' + node.data + '?>\n')
+
+    def process_text_node(self, node):
+        node.data = self.reduce_redundant_whitespace(node.data)
+        if node.data != " ":
+            if self.is_inline_element(node.parentNode):
+                node.data = node.data.strip()
+            else:
+                previous = node.previousSibling
+                if previous is None or previous.nodeType in (4, 7, 8) or \
+                        (previous.nodeType == 1 and not self.is_inline_element(previous)):
+                    node.data = node.data.lstrip()
+                    node.data = self.calculate_indentation() + node.data
+            self.result.append(node.data)
+
+    def postprocess(self):
+        self.result = self.convert_result_to_string(self.result)
+        self.result = self.remove_empty_lines(self.result)
+        self.result = self.remove_whitespace_before_end_of_line(self.result)
+        self.result = self.result.strip()
+
+    def write_to_output_file(self):
+        with open(self.output_file, 'w') as output_file:
+            output_file.write(self.result)
+
+    # Functions for processing the indentation
+    def calculate_indentation(self):
+        return self.indentation_level * self.formatting_options.indentation * ' '
+
     def decrease_indentation_level(self):
         self.indentation_level -= 1
 
     def increase_indentation_level(self):
         self.indentation_level += 1
 
-    def process_element_node(self, node):
-        self.process_element_start_tag(node)
-        for child in node.childNodes:
-            self.process(child)
-        if node.childNodes.length != 0:
-            self.process_element_end_tag(node)
-
-    def process_element_attributes(self, node):
-        if node.hasAttributes:
-            number_of_attributes = node.attributes.length
-            for i in range(number_of_attributes):
-                self.process(node.attributes.item(i))
-
-    def process_element_end_tag(self, node):
-        if self.is_inline_element(node):
-            self.result.append('</' + node.tagName + '>')
-        else:
-            self.decrease_indentation_level()
-            self.result.append('\n')
-            self.result.append(self.calculate_indentation())
-            self.result.append('</' + node.tagName + '>\n')
-
-    def process_element_start_tag(self, node):
-        if self.is_inline_element(node):
-            self.result.append('<' + node.tagName)
-            self.process_element_attributes(node)
-            if node.childNodes.length == 0:
-                self.result.append('/>')
-            else:
-                self.result.append('>')
-        else:
-            self.result.append('\n')
-            self.result.append(self.calculate_indentation())
-            self.result.append('<' + node.tagName)
-            self.process_element_attributes(node)
-            if node.childNodes.length == 0:
-                self.result.append('/>\n')
-            else:
-                self.result.append('>\n')
-                self.increase_indentation_level()
-
-    def process_processing_instruction_node(self, node):
-        text = node.data
-        text = regex.sub(r"\s+", " ", text)
-        self.result.append('<?' + node.target + ' ' + text + '?>\n')
-
-    def process_text_node(self, node):
-        text = node.data
-        text = regex.sub(r"\s+", " ", text)
-        if text != " ":
-            if self.is_inline_element(node.parentNode):
-                text = text.strip()
-            else:
-                previous = node.previousSibling
-                if previous is None or previous.nodeType in (4, 8) or \
-                        (previous.nodeType == 1 and not self.is_inline_element(previous)):
-                    text = text.lstrip()
-                    text = self.calculate_indentation() + text
-            self.result.append(text)
-
-    def postprocess(self):
-        self.result = "".join(self.result)
-        self.result = self.result.replace("\n\n", "\n")
-        self.result = regex.sub(r"\s+\n", "\n", self.result)
-        self.result = self.result.strip()
-
-    def write_to_output_file(self):
-        with open(self.output_file, 'w') as output_file:
-            output_file.write(self.result)
+    # Functions for processing the different element types
+    def is_empty_element(self, node):
+        # An element is empty if it either has no child nodes at all
+        # or if it has exactly one text node (constant: 3) with just
+        # whitespaces as a child.
+        match node.childNodes.length:
+            case 0:
+                return True
+            case 1:
+                if node.firstChild.nodeType == 3 and self.remove_whitespace(node.firstChild.data) == "":
+                    return True
+                else:
+                    return False
+            case _:
+                return False
 
     def is_inline_element(self, node):
         if node.tagName in self.formatting_options.inline_elements:
@@ -182,5 +227,24 @@ class XMLCustomFormatter:
         else:
             return False
 
-    def calculate_indentation(self):
-        return self.indentation_level * self.formatting_options.indentation * ' '
+    # Functions for processing whitespace
+    @staticmethod
+    def reduce_redundant_whitespace(string: str):
+        return regex.sub(r"\s+", " ", string)
+
+    @staticmethod
+    def remove_whitespace(string: str):
+        return regex.sub(r"\s", "", string)
+
+    @staticmethod
+    def remove_empty_lines(string: str):
+        return regex.sub(r"\n+", "\n", string)
+
+    @staticmethod
+    def remove_whitespace_before_end_of_line(string: str):
+        return regex.sub(r"\s+\n", "\n", string)
+
+    # Other functions
+    @staticmethod
+    def convert_result_to_string(result: list):
+        return "".join(result)
