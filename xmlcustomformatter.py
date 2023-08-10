764e6627bf6f46f1a027383debc8b4dbe931fe37
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-08-10 14:42:41 +0200

    Refactoring

--- xmlcustomformatter.py
+++ xmlcustomformatter.py
@@ -9,18 +9,25 @@ class XMLCustomFormatter:
         self.output_file = output_file
         self.formatting_options = formatting_options or XMLFormattingOptions()
         self.indentation_level = 0
-        self.result = []
+        self.result = None
         self.dom = None
         self.format()
 
     def format(self):
         self.read_input_file_to_dom()
+        self.add_xml_declaration()
         self.process(self.dom)
         self.postprocess()
         self.write_to_output_file()
 
     def read_input_file_to_dom(self):
         self.dom = minidom.parse(self.input_file)
+
+    def add_xml_declaration(self):
+        # This is necessary because the XML DOM does not contain
+        # an XML-declaration. If there was one present in the
+        # input document, the parser would ignore it.
+        self.result = ['<?xml version="1.0" encoding="UTF-8"?>\n']
 
     def process(self, node):
         match node.nodeType:
@@ -33,9 +40,11 @@ class XMLCustomFormatter:
             case 4:
                 self.process_cdata_node(node)
             case 5:
-                self.process_entity_reference_node(node)
+                # ToDo: Entity Reference Node
+                pass
             case 6:
-                self.process_entity_node(node)
+                # ToDo: Entity Node
+                pass
             case 7:
                 self.process_processing_instruction_node(node)
             case 8:
@@ -45,15 +54,17 @@ class XMLCustomFormatter:
             case 10:
                 self.process_document_type_node(node)
             case 11:
-                self.process_document_fragment_node(node)
+                # ToDo: Document Fragment Node
+                pass
             case 12:
-                self.process_notation_node(node)
+                # ToDo: Notation Node
+                pass
 
     def process_attribute_node(self, node):
         self.result.append(' ' + node.nodeName + '="' + node.nodeValue + '"')
 
     def process_cdata_node(self, node):
-        self.append_indentation()
+        self.result.append(self.calculate_indentation())
         self.result.append("\n" + self.calculate_indentation() + "<![CDATA[" + node.nodeValue + ']]>\n')
 
     def process_comment_node(self, node):
@@ -63,29 +74,29 @@ class XMLCustomFormatter:
         self.result.append('\n' + self.calculate_indentation() + '<!--' + comment + '-->\n')
 
     def process_document_node(self, node):
-        for child in node.childNodes:
-            self.process(child)
-
-    def process_document_fragment_node(self, node):
-        # Todo: Document Fragments
-        pass
+        for childNode in node.childNodes:
+            self.process(childNode)
 
     def process_document_type_node(self, node):
-        self.result.append("<!DOCTYPE " + node.nodeName + " ")
+        self.result.append("<!DOCTYPE " + node.name + " ")
         self.process_document_type_internal_subset(node)
         self.result.append(">")
 
     def process_document_type_internal_subset(self, node):
-        self.indentation_level += 1
         if node.internalSubset:
+            self.increase_indentation_level()
             subset = node.internalSubset
-            indentation = self.calculate_indentation()
-            self.result.append('[')
             subset = " ".join(subset.split())
-            subset = subset.replace("<!", "\n" + indentation + "<!")
-            self.result.append(subset)
-            self.result.append(']')
+            subset = subset.replace("<!", "\n" + self.calculate_indentation() + "<!")
+            subset = subset.replace("<?", "\n" + self.calculate_indentation() + "<?")
+            self.result.append('[' + subset + '\n]')
+            self.decrease_indentation_level()
+
+    def decrease_indentation_level(self):
         self.indentation_level -= 1
+
+    def increase_indentation_level(self):
+        self.indentation_level += 1
 
     def process_element_node(self, node):
         self.process_element_start_tag(node)
@@ -104,9 +115,9 @@ class XMLCustomFormatter:
         if self.is_inline_element(node):
             self.result.append('</' + node.tagName + '>')
         else:
-            self.indentation_level -= 1
+            self.decrease_indentation_level()
             self.result.append('\n')
-            self.append_indentation()
+            self.result.append(self.calculate_indentation())
             self.result.append('</' + node.tagName + '>\n')
 
     def process_element_start_tag(self, node):
@@ -119,29 +130,19 @@ class XMLCustomFormatter:
                 self.result.append('>')
         else:
             self.result.append('\n')
-            self.append_indentation()
+            self.result.append(self.calculate_indentation())
             self.result.append('<' + node.tagName)
             self.process_element_attributes(node)
             if node.childNodes.length == 0:
                 self.result.append('/>\n')
             else:
                 self.result.append('>\n')
-                self.indentation_level += 1
-
-    def process_entity_node(self, node):
-        # ToDo: Entities
-        pass
-
-    def process_entity_reference_node(self, node):
-        # ToDo: Entity References
-        pass
-
-    def process_notation_node(self, node):
-        # ToDo: Notations
-        pass
+                self.increase_indentation_level()
 
     def process_processing_instruction_node(self, node):
-        self.result.append('<?' + node.target + ' ' + node.data + '?>\n')
+        text = node.data
+        text = regex.sub(r"\s+", " ", text)
+        self.result.append('<?' + node.target + ' ' + text + '?>\n')
 
     def process_text_node(self, node):
         text = node.data
@@ -158,15 +159,14 @@ class XMLCustomFormatter:
             self.result.append(text)
 
     def postprocess(self):
-        self.result = ''.join(self.result)
+        self.result = "".join(self.result)
         self.result = self.result.replace("\n\n", "\n")
         self.result = regex.sub(r"\s+\n", "\n", self.result)
         self.result = self.result.strip()
 
     def write_to_output_file(self):
         with open(self.output_file, 'w') as output_file:
-            output_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-            output_file.write("".join(self.result))
+            output_file.write(self.result)
 
     def is_inline_element(self, node):
         if node.tagName in self.formatting_options.inline_elements:
@@ -174,9 +174,5 @@ class XMLCustomFormatter:
         else:
             return False
 
-    def append_indentation(self):
-        self.result.append(self.calculate_indentation())
-
     def calculate_indentation(self):
-        number_of_spaces = self.indentation_level * self.formatting_options.indentation
-        return number_of_spaces * ' '
+        return self.indentation_level * self.formatting_options.indentation * ' '
