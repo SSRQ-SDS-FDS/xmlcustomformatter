85962d55ef20a2e6f19bf16589311955ab59906c
ChristianSonderUniSG <christian.sonder@unisg.ch>
2023-08-23 11:03:02 +0200

    Added type hints for XMLFormattingOptions

--- xmldomformatter/options.py
+++ xmldomformatter/options.py
@@ -1,5 +1,8 @@
 class XMLFormattingOptions:
-    def __init__(self, max_line_length=80, indentation=4, inline_elements=None):
+    def __init__(self,
+            max_line_length: int = 80,
+            indentation: int = 4,
+            inline_elements = list | None):
         self.max_line_length = max_line_length
         self.indentation = indentation
         self.inline_elements = inline_elements
