9dfb175e93a3278b0915c02ab8f9686a49f34c46
chsonder <christian.sonder@trilog-verlag.de>
2025-07-24 17:14:09 +0200

    feat: moved functions to class

---
+++ src/xmlcustomformatter/stringmanipulation.py
@@ -0,0 +1,22 @@
+import re
+
+EMPTY_LINES: re.Pattern = re.compile(r"\n+")
+WHITESPACES: re.Pattern = re.compile(r"\s+")
+WHITESPACE_EOL: re.Pattern = re.compile(r"\s+\n")
+
+
+class StringManipulation:
+    def convert_list_to_string(result: list[str]):
+        return "\n".join(result)
+
+    def reduce_redundant_whitespace(string: str):
+        return WHITESPACES.sub(" ", string)
+
+    def remove_empty_lines(string: str):
+        return EMPTY_LINES.sub("\n", string)
+
+    def remove_whitespace(string: str):
+        return string.replace(" ", "")
+
+    def remove_whitespace_before_end_of_line(string: str):
+        return WHITESPACE_EOL.sub("\n", string)
